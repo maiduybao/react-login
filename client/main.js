e4d2478c20cf32ea110dc57775920ddca6ad728e
Bao Mai <bao.mai@ticketfly.com>
2017-08-29 16:16:37 -0700

    add dashboard page

--- client/main.js
+++ client/main.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+
 import "./css/main.scss";
 
 ReactDOM.render(
     <BrowserRouter>
-        <Login/>
+        <div>
+            <Route path="/" component={Login} exact={true}/>
+            <Route path="/login" component={Login} exact={true}/>
+            <Route path="/index" component={Login} exact={true}/>
+            <Route authorize={["admin"]} component={Dashboard} path="/dashboard" exact={true}/>
+        </div>
     </BrowserRouter>,
     document.getElementById("app")
 );
