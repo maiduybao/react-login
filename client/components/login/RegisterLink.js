1f7c3380ed49fd34242c8875b3c9009510f5b9b2
Bao Mai <maiduybao@gmail.com>
2017-10-02 22:54:03 -0700

    refactor function

--- client/components/login/RegisterLink.js
+++ client/components/login/RegisterLink.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-export default function RegisterLink() {
-    return (
-        <Link to="/register" className="nav-link page-login__register-link__nav-link" replace>
-            <i className="fa fa-user-plus" aria-hidden="true"/> Register
-        </Link>
-    );
-}
+const RegisterLink = () => (
+    <Link to="/register" className="nav-link page-login__register-link__nav-link" replace>
+        <i className="fa fa-user-plus" aria-hidden="true"/> Register
+    </Link>
+);
+
+export default RegisterLink;
