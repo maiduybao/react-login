80fc903eb2a1f46036549b1d69774f3130ccc4c6
Bao Mai <maiduybao@gmail.com>
2017-11-10 20:18:16 -0800

    add correct fontawesome i tag

--- client/components/login/RegisterLink.js
+++ client/components/login/RegisterLink.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 const RegisterLink = () => (
     <div className="page-login__register-link">
         <Link to="/register" className="nav-link page-login__register-link__nav-link" replace>
-            <i className="fa fa-user-plus" aria-hidden="true"/> Register
+            <i className="fa fa-user-plus" aria-hidden="true"></i> Register
         </Link>
     </div>
 );
